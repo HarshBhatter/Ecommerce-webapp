054425b6d518077d14f56b2f017ecf3da3f9ec39
HarshBhatter <harshbhatter03@gmail.com>
2026-01-15 02:26:48 +0530

    spell error corrected

--- frontend/my_ecom/src/Individualproduct/Individualproduct.jsx
+++ frontend/my_ecom/src/Individualproduct/Individualproduct.jsx
@@ -1,5 +1,5 @@
 import React, { useState , useEffect} from 'react'
-import './Individualproduct.css'
+import './IndividualProduct.css'
 import { Products } from '../Products/Products'
 import { IndividualProductData } from './IndividualProductData'
 import { useParams } from 'react-router-dom'
